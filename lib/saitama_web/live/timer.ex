9fcd393ff1da381e709ad61b69f91a19b2043166
T Floyd Wright <tfwright@gmail.com>
2020-01-28 00:09:43 -0700

    Support multiple intervals

--- lib/saitama_web/live/timer.ex
+++ lib/saitama_web/live/timer.ex
@@ -4,33 +4,47 @@ defmodule SaitamaWeb.Live.Timer do
   def mount(_session, socket) do
     socket =
       socket
+      |> assign(:intervals, [])
       |> assign(:control, "start")
-      |> assign(:time_in_secs, nil)
+      |> assign(:current_interval, 0)
 
     {:ok, socket}
   end
 
   def render(assigns) do
     ~L"""
-      <%= @time_in_secs %>
-      <%= if @time_in_secs do %>
-        <%= if @time_in_secs > 0 do %>
-          <button phx-click=<%= @control %>><%= @control %></button>
-        <% end %>
-        <%= if @control == "start" do %>
-          <button phx-click="reset">reset</button>
-        <% end %>
-      <% else %>
-        <form phx-submit="set_timer">
-          <input type="number" name="duration" min="1"></input>
-          <input type="submit"/>
-        </form>
+      <%= for %{"label" => label, "duration" => duration} <- @intervals do %>
+        <p><%= label %>: <%= duration %></p>
       <% end %>
+
+      <%= if @intervals |> Enum.any? do %>
+        <button phx-click=<%= @control %>><%= @control %></button>
+      <% end %>
+
+      <hr />
+
+      <form phx-submit="add_interval">
+        <label>label<label>
+        <input type="text" name="label" required />
+        <label>duration<label>
+        <input type="number" name="duration" min="1" required />
+        <input type="submit" value="Add">
+      </form>
     """
   end
 
-  def handle_event("set_timer", %{"duration" => duration}, socket) do
-    socket = assign(socket, :time_in_secs, String.to_integer(duration))
+  def handle_event(
+        "add_interval",
+        %{"label" => _label, "duration" => _duration} = new_interval,
+        socket
+      ) do
+    socket =
+      assign(
+        socket,
+        :intervals,
+        [new_interval |> Map.update!("duration", &String.to_integer/1) | socket.assigns.intervals]
+        |> Enum.reverse()
+      )
 
     {:noreply, socket}
   end
@@ -51,22 +65,32 @@ defmodule SaitamaWeb.Live.Timer do
     {:noreply, socket}
   end
 
-  def handle_event("reset", _event, socket) do
-    send(self(), :stop)
-
-    socket = assign(socket, :time_in_secs, nil)
-
-    {:noreply, socket}
-  end
-
   def handle_info(:tick, socket) do
-    time_remaining = socket.assigns.time_in_secs - 1
-
-    if time_remaining == 0 do
+    if !(socket.assigns.intervals |> Enum.any?(&(&1["duration"] > 0))) do
       send(self(), :stop)
     end
 
-    {:noreply, assign(socket, time_in_secs: time_remaining)}
+    current_interval = socket.assigns.current_interval
+
+    new_intervals =
+      socket.assigns.intervals
+      |> Enum.with_index()
+      |> Enum.map(fn {interval, index} ->
+        if index == current_interval do
+          interval |> Map.update!("duration", &(&1 - 1))
+        else
+          interval
+        end
+      end)
+
+    socket =
+      case new_intervals |> Enum.at(current_interval) do
+        %{"duration" => 0} -> assign(socket, :current_interval, current_interval + 1)
+        _ -> socket
+      end
+      |> assign(:intervals, new_intervals)
+
+    {:noreply, socket}
   end
 
   def handle_info(:stop, socket) do

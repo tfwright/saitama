f798cbf8799706b7a69d13f009048a36283c83dc
T Floyd Wright <tfwright@gmail.com>
2020-03-08 16:49:56 -0500

    Configure render

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -7,8 +7,8 @@ use Mix.Config
 config :saitama, SaitamaWeb.Endpoint,
   # Possibly not needed, but doesn't hurt
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "", port: 80],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   server: true
 
 config :saitama, Saitama.Repo,
